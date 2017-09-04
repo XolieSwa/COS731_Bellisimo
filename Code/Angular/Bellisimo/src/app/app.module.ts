bcbfef7c1d8e68db75b8d70dc25e834079f8cf98
Xoliswa Ntshingila <x.ntshingila@gmail.com>
2017-09-04 16:44:48 +0200

    Add components

--- Code/Angular/Bellisimo/src/app/app.module.ts
+++ Code/Angular/Bellisimo/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClothingComponent } from './clothing/clothing.component';
+import { FoodComponent } from './food/food.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClothingComponent,
+    FoodComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
