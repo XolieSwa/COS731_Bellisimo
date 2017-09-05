088b1f395b7ccddcab9674793430087644e9a42a
Xoliswa Ntshingila <x.ntshingila@gmail.com>
2017-09-05 17:08:49 +0200

    Added backend

---
+++ Code/Angular/bellisimo-backend/src/main/java/com/softwareengieering/bellisimobackend/BellisimoBackendApplication.java
@@ -0,0 +1,12 @@
+package com.softwareengieering.bellisimobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellisimoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellisimoBackendApplication.class, args);
+	}
+}
